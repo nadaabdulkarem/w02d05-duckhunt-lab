d6295f72656fd4a784c952bcecdca3c3ac9d5ae7
nadaabdulkarem <asghar.nada@gmail.com>
2018-11-24 22:19:12 +0300

    homework part 2 and 3

--- duckhunt.js
+++ duckhunt.js
@@ -10,13 +10,13 @@ $(document).ready(function() {
     for (var i = 0; i < 5; i++){
       $duck[i] = $('<div/>').addClass('duck');
       $body.append($duck);
-    }
+    };
 
     $('.duck').each(function(){
       $(this).css({
         'top' : Math.floor(Math.random() * window.innerHeight) + 'px',
         'left' : Math.floor(Math.random() * window.innerWidth) + 'px'          
-      })
+      });
     });
     
     setInterval(function(){
@@ -30,10 +30,10 @@ $(document).ready(function() {
         $(this).animate({
           'top' : Math.floor(Math.random() * window.innerHeight) + 'px',
           'left' : Math.floor(Math.random() * window.innerWidth) + 'px'          
-        })
+        });
       });
     }, 1000 , 'linear');
-
+    
     $('.duck').on('click', function(){
       $(this).removeClass('duck').addClass('shot');
 
@@ -42,31 +42,20 @@ $(document).ready(function() {
         backgroundImage : 'url(images/shot.png)',
         width : '100px',
         height : '95px',
-        })
+        });
 
       setTimeout(function(){
         $('.shot').remove();
+        checkForWinner();
       }, 1000);
+      
     });
-    
-    return $duck;
   }
   createDuck();
 
   function checkForWinner() {
     if($('.duck').length == 0){
       alert("YOU WIN!");
-    } else {
-      setTimeout(checkForWinner, 1000);
-    }
+     }
   }
-  checkForWinner();
-
-  // 14. BONUS: The ducks are moving pretty erratically, can you think
-  //     of a way to adjust the ducks speed based on how far needs to move?
-
-  // 15. BONUS: Add the "left" and "right" class to the duck based on the
-  //     direction the duck is flying
-  
-
 })
